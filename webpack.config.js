c1e81903f7256300878235cee298fa9a35e9dbdb
gtsoul-tech <56584633+gtsoul-tech@users.noreply.github.com>
2023-08-30 13:03:30 +0300

    Add css and plugin for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
     index: './src/index.js',
     initialLoad: './src/initialLoad.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
